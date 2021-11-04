686977816e1bfce682fc03e61bfac79d4a995e7c
Viktor Bondar <vktrbndr@gmail.com>
2021-11-05 00:36:28 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('#ingredients');
+
+const createListItems = function (array) {
+  
+  for (const element of array) {
+  const itemList = document.createElement('li');
+  itemList.textContent = element;
+  itemList.classList = 'item';
+  list.append(itemList)
+  }
+  return;
+}
+
+createListItems(ingredients);
+
+
+
+
