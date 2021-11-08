e7c4a21e2ef7b5e25dceeeb8af7565f627a263a1
Viktor Bondar <vktrbndr@gmail.com>
2021-11-09 01:35:49 +0200

    task-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,10 @@
+const form = document.querySelector('.login-form');
+const btn = form.lastElementChild;
+console.log(btn);
 
+
+form.addEventListener('submit', (event) => {
+    
+    
+    console.log('event:', event);
+})
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const button = document.querySelector('.change-color');
+const body = document.querySelector('.body');
+const colorName = document.querySelector('.color');
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeBgColor() {
+  let color = getRandomHexColor();
+  colorName.textContent = `${color}`;
+  body.style.backgroundColor = `${color}`;
+   
+}
+
+button.onclick = changeBgColor;
+
